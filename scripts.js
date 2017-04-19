3fe61d28068dc7dca08f91058779775a1e814f6b
Mikkal McNulty <mcnultymikkal@gmail.com>
2017-04-18 19:11:12 -0700

    back space and clear functions now work

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-var typeNum = function() {
-	document.getElementById('total').value = "Hello World";
-}
+var totalNum = 0;
+
+	function clearAnswer() {
+		document.getElementById('answer').value = '';
+	}
+
+	function backSpace() {
+		var text = document.getElementById('answer').value;
+		text = text.slice(0,text.length-1);
+		document.getElementById('answer').value = text;
+	}
+
+	function evaluate() {
+		eval(document.getElementById('answer').value);
+	}
